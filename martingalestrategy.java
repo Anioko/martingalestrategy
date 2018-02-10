91ae46be49fd225b86f11a36cea5c1122f3d19a4
anioko1 <aniekanokono@gmail.com>
2018-02-10 08:32:51 +0000

    Create martingalestrategy.java

---
+++ martingalestrategy.java
@@ -0,0 +1,25 @@
+
+
+
+
+public int martingale()
+{
+    int money = 1000;
+    int target = 1200;
+    int bet = 10;
+    while(money>bet)
+    {
+        boolean win = play();
+        if(win)
+        {
+            money += bet;
+            bet = 10;
+        }
+        else 
+        {
+            money -= bet;
+            bet *= 2;
+        }
+    }
+    return money;
+}
